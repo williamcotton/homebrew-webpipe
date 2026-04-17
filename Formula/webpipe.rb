21b46271ac44cef81402409ef6897914dae1b94a
William Cotton <williamc@eurekasoft.com>
2026-04-17 07:37:53 -0500

    v2.0.1

--- Formula/webpipe.rb
+++ Formula/webpipe.rb
@@ -1,17 +1,20 @@
-class Webpipe < Formula
-  desc "Pipeline-based web API runtime"
-  homepage "https://github.com/williamcotton/webpipe-rs"
-  url "https://github.com/williamcotton/webpipe-rs.git", tag: "v2.0.0"
-  license "MIT"
-  head "https://github.com/williamcotton/webpipe-rs.git", branch: "main"
+# frozen_string_literal: true
 
-  depends_on "rust" => :build
+# Webpipe formula for Homebrew
+class Webpipe < Formula
+  desc 'Pipeline-based web API runtime'
+  homepage 'https://github.com/williamcotton/webpipe'
+  url 'https://github.com/williamcotton/webpipe.git', tag: 'v2.0.1'
+  license 'MIT'
+  head 'https://github.com/williamcotton/webpipe.git', branch: 'main'
+
+  depends_on 'rust' => :build
 
   def install
-    system "cargo", "install", *std_cargo_args
+    system 'cargo', 'install', *std_cargo_args
   end
 
   test do
-    assert_match "webpipe", shell_output("#{bin}/webpipe --version")
+    assert_match 'webpipe', shell_output("#{bin}/webpipe --version")
   end
 end
