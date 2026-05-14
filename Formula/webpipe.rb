e90f9c6ef3289928b8259f1e3b216613de75fb0e
William Cotton <williamc@eurekasoft.com>
2026-05-14 07:15:59 -0500

    v2.0.87

--- Formula/webpipe.rb
+++ Formula/webpipe.rb
@@ -4,7 +4,7 @@
 class Webpipe < Formula
   desc 'Pipeline-based web API runtime'
   homepage 'https://github.com/williamcotton/webpipe'
-  url 'https://github.com/williamcotton/webpipe.git', tag: 'v2.0.82'
+  url 'https://github.com/williamcotton/webpipe.git', tag: 'v2.0.87'
   license 'MIT'
   head 'https://github.com/williamcotton/webpipe.git', branch: 'main'
 
